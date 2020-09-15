c8563ca4028cda6d7f30c193753c810ac0050e99
Taylor <tayrichie@users.noreply.github.com>
2020-09-15 10:51:01 -0400

    Webinar Updates

--- src/app/shared/pages/page-title/page-title.component.ts
+++ src/app/shared/pages/page-title/page-title.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class PageTitleComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // document.title = "This is a page title!";
+  }
 }
